3b6300aaa0bb1aa43d595c0bdd98bc7675cff639
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-04-02 01:12:22 +0500

    File of q58

---
+++ q58.js
@@ -0,0 +1,12 @@
+//Average Score Calculator: Write a simple program that can take lots of scores and find their average.
+// This program calculates the average of all scores given
+function averageScore() {
+    var scores = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        scores[_i] = arguments[_i];
+    }
+    // Adds all scores together and divides by the number of scores
+    var total = scores.reduce(function (sum, score) { return sum + score; }, 0);
+    return total / scores.length;
+}
+console.log(averageScore(80, 90, 100, 70));
